28c341ad2f2cb09699d6777d323289d894cff830
黄浩 <zhicil@>
2022-04-25 11:00:46 +0800

    123

--- src/main/java/com/hz/springsecurity02/pojo/SmbmsUser.java
+++ src/main/java/com/hz/springsecurity02/pojo/SmbmsUser.java
@@ -13,7 +13,7 @@ public class SmbmsUser {
 	private String phone;   //电话
 	private String address; //地址
 	private Integer userRole;    //用户角色
-	private Integer created;   //创建者
+	private Integer createdBy;   //创建者
 	private String creationDate; //创建时间
 	private Integer modifyBy;     //更新者
 	private String modifyDate;   //更新时间
