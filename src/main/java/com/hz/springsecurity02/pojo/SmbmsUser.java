aa41c69bac150860b0d466a80f5f7b844188a5b4
黄浩 <zhicil@>
2022-04-25 10:59:25 +0800

    123

---
+++ src/main/java/com/hz/springsecurity02/pojo/SmbmsUser.java
@@ -0,0 +1,21 @@
+package com.hz.springsecurity02.pojo;
+
+import lombok.Data;
+
+@Data
+public class SmbmsUser {
+	private Integer id; //id 
+	private String userCode; //用户编码
+	private String userName; //用户名称
+	private String userPassword; //用户密码
+	private Integer gender;  //性别
+	private String birthday;  //出生日期
+	private String phone;   //电话
+	private String address; //地址
+	private Integer userRole;    //用户角色
+	private Integer created;   //创建者
+	private String creationDate; //创建时间
+	private Integer modifyBy;     //更新者
+	private String modifyDate;   //更新时间
+
+}
